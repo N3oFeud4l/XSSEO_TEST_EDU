e52cf8b682b870dd65e48b2f02c518aba9df88a7
dlawlsdud0419 <59009654+dlawlsdud0419@users.noreply.github.com>
2025-05-02 06:53:33 +0900

    Update XSSEOload.js

--- xml/XSSEOload.js
+++ xml/XSSEOload.js
@@ -2,6 +2,7 @@ const queryParams = new URLSearchParams(window.location.search);
 const id = queryParams.get('id');
 
 if (id) {
+  console.log("실행");
   fetch('XSSEOViewer.html' + '?' + id)
     .then(response => response.text())
     .then(data => {
