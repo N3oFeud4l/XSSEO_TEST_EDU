aeff9fafcbc41e78890406ad1770c735b25835ac
N3oFeud4l <59009654+N3oFeud4l@users.noreply.github.com>
2025-05-12 10:44:04 +0900

    Update XSSEOload.js

--- xml/XSSEOload.js
+++ xml/XSSEOload.js
@@ -1,28 +1,3 @@
-/* const queryParams = new URLSearchParams(window.location.search);
-const id = queryParams.get('id');
-
-if (id) {
-  console.log("실행");
-  fetch('XSSEOViewer.html' + '?' + id)
-    .then(response => response.text())
-    .then(data => {
-      try {
-        let r = JSON.parse(data);
-        if (r.redirectUrl) {
-          window.location.href = r.redirectUrl;
-        }
-      } catch (e) {
-        document.open();
-        document.write(data);
-        document.close();
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-} else {
-  console.error('No ID parameter found.');
-} */
 fetch('XSSEOViewer.html')
   .then(response => response.text())
   .then(data => {
